8b947ea61cb48a65ee10606f395e4ea765f9aa62
Andiiiiie <andiejessica2020@gmail.com>
2022-11-25 09:39:26 +0300

    a

---
+++ src/Client.java
@@ -0,0 +1,58 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+
+
+    Socket socket;
+    DataInputStream lecture;
+    DataOutputStream envoi;
+    public Client(String adresse,int port) throws IOException {
+        this.setSocket(new Socket(adresse,port));
+        this.setEnvoi(new DataOutputStream(this.getSocket().getOutputStream()));
+        this.setLecture(new DataInputStream(this.getSocket().getInputStream()));
+    }
+
+    public void sendRequete(String requete) throws IOException {
+        this.getEnvoi().writeUTF(requete);
+        this.getEnvoi().flush();
+    }
+
+    public void afficherReponse() throws IOException {
+        System.out.println(this.getLecture().readUTF());
+    }
+
+    public void exit() throws IOException {
+        this.getLecture().close();
+        this.getEnvoi().close();
+        this.getSocket().close();
+    }
+
+
+    //get and set
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+    public DataInputStream getLecture() {
+        return lecture;
+    }
+
+    public void setLecture(DataInputStream lecture) {
+        this.lecture = lecture;
+    }
+
+    public DataOutputStream getEnvoi() {
+        return envoi;
+    }
+
+    public void setEnvoi(DataOutputStream envoi) {
+        this.envoi = envoi;
+    }
+
+}
