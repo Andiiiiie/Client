cb1bebf918e8413f574370ee23b2d69160289a0e
jess <andiejessica2020@gmail.com>
2022-12-19 23:23:57 +0300

    _exit

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Client client=new Client("localhost",111);
+        Client client=new Client("localhost",115);
         Scanner scanner=new Scanner(System.in);
         while (true)
         {
             System.out.print("Requete>> ");
             client.sendRequete(scanner.nextLine());
             String reponse=client.afficherReponse();
-            System.out.print("Reponse:\t"+reponse);
+            System.out.print("Reponse:\t"+reponse+"\n");
             if(reponse.equals("sortie")){break;}
         }
     }
